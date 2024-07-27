d30ccd42c34ab53999defdf1f98d41c176fb61be
ErickDev2805 <erickdiaz2805@gmail.com>
2024-07-27 12:15:50 -0600

    Add files via upload

---
+++ ConexionBD/src/conexionbd/ConexionBD.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexionbd;
+
+import controllers.Conexion;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author Lab-1 Pc-13
+ */
+public class ConexionBD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            // TODO code application logic here
+            Conexion conexion = new Conexion ();
+            Connection conn = conexion.obtenerConexion();
+            PreparedStatement ps  =
+                    conn.prepareStatement("Select * from ciudades");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next())
+            {
+                System.out.println("Conexion");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ ConexionBD/src/models/City.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Lab-1 Pc-13
+ */
+public class City {
+  private int id;
+  private String Nombre;
+  private boolean activo;
+
+    public City() {
+    }
+
+    public City(int id, String Nombre, boolean activo) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.activo = activo;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+  
+}
